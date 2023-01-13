e49f66b8064e5b735a4ac453d0d5fd8a8927b120
Chris Reske <chris.reske@outlook.de>
2023-01-13 12:54:29 +0100

    Add first model class ('Customer').

---
+++ PetClinicWebApp/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace PetClinicWebApp.Api.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public int Age { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email  { get; set; }
+    public string Street { get; set; }
+    public string ZipCode { get; set; }
+    public string City { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime LastChangedAt { get; set; }
+}
